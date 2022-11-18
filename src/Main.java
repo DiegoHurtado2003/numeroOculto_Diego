97d23aa0cc3ba7ddf5e426a457504ead6dbdd861
DiegoHurtado2003 <diego.hurtado@iesnervion.es>
2022-11-18 20:56:03 +0100

    Commit1

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+
+        numeroOculto hilo1 = new numeroOculto();
+        hilo1.start();
+
+
+        numeroOculto hilo2 = new numeroOculto();
+        hilo2.start();
+
+        numeroOculto hilo3 = new numeroOculto();
+        hilo3.start();
+
+        numeroOculto hilo4 = new numeroOculto();
+        hilo4.start();
+
+        numeroOculto hilo5 = new numeroOculto();
+        hilo5.start();
+
+        numeroOculto hilo6 = new numeroOculto();
+        hilo6.start();
+
+        numeroOculto hilo7 = new numeroOculto();
+        hilo7.start();
+
+        numeroOculto hilo8 = new numeroOculto();
+        hilo8.start();
+
+        numeroOculto hilo9 = new numeroOculto();
+        hilo9.start();
+
+        numeroOculto hilo10 = new numeroOculto();
+        hilo10.start();
+
+    }
+
+
+}
